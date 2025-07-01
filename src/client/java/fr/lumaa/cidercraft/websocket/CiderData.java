43520cacb2e88056473efb01004ba3770bb02818
lumaa-dev <lumaa@lumaa.fr>
2025-07-01 15:02:39 +0200

    fix #1

--- src/client/java/fr/lumaa/cidercraft/websocket/CiderData.java
+++ src/client/java/fr/lumaa/cidercraft/websocket/CiderData.java
@@ -7,17 +7,12 @@ import fr.lumaa.cidercraft.data.CiderCraftConfig;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import org.jetbrains.annotations.Nullable;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.*;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 public class CiderData {
@@ -33,7 +28,7 @@ public class CiderData {
     public CompletableFuture<Boolean> isActive() {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                URL url = new URI("http://localhost:10767/api/v1/playback/active").toURL();
+                URL url = new URI(this.reqUrl + "/api/v1/playback/active").toURL();
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("GET");
                 conn.setConnectTimeout(2000);
@@ -48,9 +43,14 @@ public class CiderData {
                 } else {
                     CiderCraft.LOGGER.error("[CiderCraft] Cider is currently inactive");
                 }
-            } catch (Exception e) {
+            } catch (ProtocolException e) {
                 e.printStackTrace();
-                CiderCraft.LOGGER.error("[CiderCraft] Received error from /v1/playback/active");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             return false;
         });
@@ -68,56 +68,50 @@ public class CiderData {
                 options.transports = new String[]{"websocket"};
                 options.extraHeaders = Map.of("apptoken", List.of(this.token));
 
-                Socket socket = IO.socket("http://localhost:10767", options);
+                Socket socket = IO.socket(this.reqUrl, options);
 
                 socket.on(Socket.EVENT_CONNECT, args -> CiderCraft.LOGGER.info("[CiderCraft] Socket.IO connected"));
 
                 socket.on("API:Playback", args -> {
-                    if (args.length > 0 && args[0] instanceof JSONObject playbackData) {
-                        String type = playbackData.optString("type", "unknown");
+                    if (args.length > 0 && args[0].toString() instanceof String jsonString) {
+                        try {
+                            JsonObject playbackData = JsonParser.parseString(jsonString).getAsJsonObject();
+                            String type = playbackData.has("type") ? playbackData.get("type").getAsString() : "unknown";
 
-                        switch (type) {
-                            case "playbackStatus.nowPlayingItemDidChange":
-                                try {
-                                    this.currentTrack = this.findCurrentTrack(playbackData, "data");
+                            switch (type) {
+                                case "playbackStatus.nowPlayingItemDidChange" -> {
+                                    this.currentTrack = this.findCurrentTrack(playbackData.getAsJsonObject("data"));
                                     CiderCraft.LOGGER.info("[CiderCraft] Changed to new track: {}", this.currentTrack.title);
-                                } catch (JSONException e) {
-                                    CiderCraft.LOGGER.error("[CiderCraft] Error occured on \"" + type + "\" with data: " + playbackData);
-                                    throw new RuntimeException(e);
                                 }
-                                break;
-                            case "playbackStatus.playbackTimeDidChange":
-                                if (this.currentTrack != null) {
-                                    try {
-                                        JSONObject data = playbackData.getJSONObject("data");
-                                        float currentTime = (float) data.optDouble("currentPlaybackTime", 0.0);
-                                        float duration = (float) data.optDouble("currentPlaybackDuration", 0.0);
+                                case "playbackStatus.playbackTimeDidChange" -> {
+                                    if (this.currentTrack != null && playbackData.has("data")) {
+                                        JsonObject data = playbackData.getAsJsonObject("data");
+                                        float currentTime = data.has("currentPlaybackTime") ? data.get("currentPlaybackTime").getAsFloat() : 0.0f;
+                                        float duration = data.has("currentPlaybackDuration") ? data.get("currentPlaybackDuration").getAsFloat() : 0.0f;
 
                                         this.currentTrack.setTime(currentTime, duration);
-                                    } catch (JSONException e) {
-                                        throw new RuntimeException(e);
                                     }
                                 }
-                                break;
-                            case "playbackStatus.playbackStateDidChange":
-                                try {
-                                    JSONObject data = playbackData.getJSONObject("data");
-                                    boolean playing = Objects.equals(data.optString("currentPlaybackTime", "unknown"), "playing");
+                                case "playbackStatus.playbackStateDidChange" -> {
+                                    JsonObject data = playbackData.getAsJsonObject("data");
+                                    boolean playing = data.has("currentPlaybackTime") && "playing".equals(data.get("currentPlaybackTime").getAsString());
 
                                     if (this.currentTrack == null) {
-                                        this.currentTrack = this.findCurrentTrack(data, "attributes");
+                                        this.currentTrack = this.findCurrentTrack(data.getAsJsonObject("attributes"));
                                         CiderCraft.LOGGER.info("[CiderCraft] Detected current track: {}", this.currentTrack.title);
                                     }
 
                                     this.currentTrack.isPlaying = playing;
-                                } catch (JSONException e) {
-                                    throw new RuntimeException(e);
                                 }
-                            default:
-//                                CiderCraft.LOGGER.info("[CiderCraft] Unhandled event type: {}", type);
+                                default -> {
+                                    // CiderCraft.LOGGER.info("[CiderCraft] Unhandled event type: {}", type);
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
                     } else {
-                        CiderCraft.LOGGER.warn("[CiderCraft] Invalid playback data received");
+                        CiderCraft.LOGGER.warn("[CiderCraft] Invalid playback data received: " + args[0].toString());
                     }
                 });
 
@@ -128,11 +122,10 @@ public class CiderData {
         });
     }
 
-    private Track findCurrentTrack(JSONObject object, String objName) throws JSONException {
-        JSONObject attributes = object.getJSONObject(objName);
-        String album = attributes.optString("albumName", "Unknown");
-        String title = attributes.optString("name", "Unknown");
-        String artist = attributes.optString("artistName", "Unknown");
+    private Track findCurrentTrack(JsonObject object) {
+        String album = object.get("albumName").getAsString();
+        String title = object.get("name").getAsString();
+        String artist = object.get("artistName").getAsString();
 
         return new Track(title, artist, album);
     }
